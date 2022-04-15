4806a7b200eacd7869881375180e3239d53589e9
lessw2020 <lessw@etrillium.com>
2022-04-15 00:22:50 +0000

    added t5 integration

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import os
 import argparse
+from policies import mixed_precision
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -10,7 +11,13 @@ from torchvision import datasets, transforms
 
 
 from transformers import AutoModelForSeq2SeqLM, AutoModelForSequenceClassification
-from transformers import AutoTokenizer
+from transformers import (
+    AutoModelForSeq2SeqLM,
+    AutoModelForSequenceClassification,
+    AutoModelForCausalLM,
+)
+from transformers import AutoTokenizer  # , GPT2TokenizerFast
+from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 import functools
 from torch.optim.lr_scheduler import StepLR
@@ -36,6 +43,9 @@ from torch.distributed.fsdp.wrap import (
 from datasets import load_dataset, load_metric
 from torch.utils.data import DataLoader
 from pathlib import Path
+from torch.utils.data import DataLoader
+from nlp import load_metric
+from nlp import load_dataset
 
 from sklearn.model_selection import train_test_split
 import time
@@ -43,9 +53,10 @@ import time
 # local imports
 import verify
 import policies
+from wikihow_dataset import wikihow
 
 # some globals
-g_port = "12369"
+g_port = "12368"
 g_addr = "localhost"
 
 
@@ -60,6 +71,13 @@ def parse_args():
     )
     parser.add_argument(
         "--seed", type=int, default=1, metavar="S", help="random seed (default: 2022)"
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=1,
+        metavar="N",
+        help="number of epochs to train (default: 14)",
     )
     args = parser.parse_args()
     return args
@@ -111,7 +129,86 @@ def setup_tasks(rank, world_size):
     # set_printing()
     setup_environ_flags()
 
-    # ---- fsdp main ------------------------------------------------------------
+
+# ----------  Training ----------------------------------------------------------
+def train(args, model, rank, world_size, train_loader, optimizer, epoch, sampler=None):
+    model.train()
+    ddp_loss = torch.zeros(2).to(rank)
+
+    if sampler:
+        sampler.set_epoch(epoch)
+    for batch in train_loader:
+        for key in batch.keys():
+            batch[key] = batch[key].to(rank)
+
+        """print("************************")
+        print(
+            "train_loader",
+            type(batch),
+            batch["source_ids"].size(),
+            batch["source_mask"].size(),
+            batch["target_ids"].size(),
+        )
+        print("************************")
+        """
+        optimizer.zero_grad()
+        output = model(
+            input_ids=batch["source_ids"],
+            attention_mask=batch["source_mask"],
+            labels=batch["target_ids"],
+        )
+        # print("##############################")
+        # print(output.keys())
+        # print("##############################")
+        loss = output["loss"]
+        loss.backward()
+        optimizer.step()
+        ddp_loss[0] += loss.item()
+        ddp_loss[1] += len(batch)
+
+    dist.reduce(ddp_loss, 0, op=dist.ReduceOp.SUM)
+    if rank == 0:
+        print("Train Epoch: {} \tLoss: {:.6f}".format(epoch, ddp_loss[0] / ddp_loss[1]))
+
+
+# ---- Validation ---------------
+
+
+def test(model, rank, world_size, test_loader):
+    model.eval()
+    correct = 0
+    ddp_loss = torch.zeros(3).to(rank)
+    with torch.no_grad():
+        for batch in test_loader:
+            for key in batch.keys():
+                batch[key] = batch[key].to(rank)
+            output = model(
+                input_ids=batch["source_ids"],
+                attention_mask=batch["source_mask"],
+                labels=batch["target_ids"],
+            )
+            ddp_loss[0] += output["loss"].item()  # sum up batch loss
+            pred = output.logits.argmax(
+                dim=1, keepdim=True
+            )  # get the index of the max log-probability
+            ddp_loss[1] += pred.eq(batch["target_ids"].view_as(pred)).sum().item()
+            ddp_loss[2] += len(batch)
+
+    dist.reduce(ddp_loss, 0, op=dist.ReduceOp.SUM)
+
+    if rank == 0:
+        test_loss = ddp_loss[0] / ddp_loss[2]
+        print(
+            "Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n".format(
+                test_loss,
+                int(ddp_loss[1]),
+                int(ddp_loss[2]),
+                100.0 * ddp_loss[1] / ddp_loss[2],
+            )
+        )
+
+
+# ---- fsdp main ------------------------------------------------------------
 
 
 def fsdp_main(rank, world_size, args):
@@ -119,6 +216,8 @@ def fsdp_main(rank, world_size, args):
     setup_tasks(rank, world_size)
 
     fsdp_unit_params = 1000000
+    batch_size = 4
+    test_batch_size = 4
 
     mp_policy, wrapping_policy = get_policies(fsdp_unit_params)
 
@@ -131,9 +230,106 @@ def fsdp_main(rank, world_size, args):
     # google/t5-v1_1-xl  #3b
     # google/t5-v1_1-xxl #11b
 
+    model = T5ForConditionalGeneration.from_pretrained(model_name)
+    tokenizer = T5Tokenizer.from_pretrained(model_name)
+    dataset_name = "wikihow"
+
+    dataset = load_dataset(dataset_name, "all", data_dir="../Fusion/data/wikihow")
+
     if rank == 0:
         print(f"--> Training for {model_name}")
+        total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+        print(f"\n--> {model_name} has {total_params/1e6} Million params\n")
 
+        print(f"{dataset_name} contains: {dataset.keys()}")
+        print("Size of {dataset_name} train dataset: ", dataset["train"].shape)
+        print(
+            "Size of {dataset_name} Validation dataset: ", dataset["validation"].shape
+        )
+
+    tokenizer = T5Tokenizer.from_pretrained(model_name)
+    train_dataset = wikihow(tokenizer, "train", None, 512, 150, True)
+    val_dataset = wikihow(tokenizer, "validation", None, 512, 150, True)
+
+    sampler1 = DistributedSampler(
+        train_dataset, rank=rank, num_replicas=world_size, shuffle=True
+    )
+    sampler2 = DistributedSampler(val_dataset, rank=rank, num_replicas=world_size)
+
+    train_kwargs = {"batch_size": batch_size, "sampler": sampler1}
+    test_kwargs = {"batch_size": test_batch_size, "sampler": sampler2}
+    cuda_kwargs = {"num_workers": 2, "pin_memory": True, "shuffle": False}
+    train_kwargs.update(cuda_kwargs)
+    test_kwargs.update(cuda_kwargs)
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, **train_kwargs)
+    test_loader = torch.utils.data.DataLoader(val_dataset, **test_kwargs)
+
+    torch.cuda.set_device(rank)
+
+    # init_start_event = torch.cuda.Event(enable_timing=True)
+    # init_end_event = torch.cuda.Event(enable_timing=True)
+
+    # init_start_event.record()
+
+    # model = model.to(rank)
+    # model = DDP(model)
+    model = FSDP(
+        model,
+        auto_wrap_policy=wrapping_policy,
+        mixed_precision=mp_policy,
+    ).to(rank)
+
+    if rank == 0:
+        print(f"model ")
+        fn = printable_model_name + "-shared_layout.txt"
+        with open(fn, "w") as external_file:
+            header_text = (
+                f"model = {model_name}, sharded with {fsdp_unit_params} parameters\n"
+            )
+            print(header_text, file=external_file)
+            total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+            milli_params = total_params * 4 / 1e6
+            print(f"\n--> {model_name} has {milli_params} params\n", file=external_file)
+            print(f"model wrapping = \n{model}\n\n", file=external_file)
+
+            external_file.close()
+
+    lr = 0.0008
+    gamma = 0.7
+    optimizer = optim.AdamW(model.parameters(), lr=lr)
+
+    scheduler = StepLR(optimizer, step_size=1, gamma=gamma)
+
+    for epoch in range(1, args.epochs + 1):
+        train(
+            args,
+            model,
+            rank,
+            world_size,
+            train_loader,
+            optimizer,
+            epoch,
+            sampler=sampler1,
+        )
+        test(model, rank, world_size, test_loader)
+        scheduler.step()
+
+    # init_end_event.record()
+
+    if rank == 0:
+        # print(
+        # f"Cuda event elapsed time: {init_start_event.elapsed_time(init_end_event) / 1000}sec"
+        # )
+        # print(f"{model}")
+
+        # save block
+        """if args.save_model:
+            dist.barrier()
+            states = model.state_dict()
+        if rank == 0:
+            torch.save(states, "t5_small_wikihow.pt")
+        """
     dist.barrier()
     cleanup()
 
