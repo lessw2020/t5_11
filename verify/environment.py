22f400f3efa99e62e2a131057de56ee2c595e2cf
Less Wright <lessw@etrillium.com>
2022-04-21 14:19:58 -0700

    add torch bfloat cuda check

--- verify/environment.py
+++ verify/environment.py
@@ -7,6 +7,7 @@ from distutils.version import LooseVersion
 
 bf16_ready = (
     torch.version.cuda
+    and torch.cuda.is_bf16_supported() 
     and LooseVersion(torch.version.cuda) >= "11.0"
     and dist.is_nccl_available()
     and nccl.version() >= (2, 10)
