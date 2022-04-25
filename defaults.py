985448f651f6f30346b781f0d55ae960781fba17
Less Wright <lessw@etrillium.com>
2022-04-25 12:45:31 -0700

    Update defaults.py

--- defaults.py
+++ defaults.py
@@ -1,6 +1,3 @@
-from dataclasses import dataclass
-
-
 @dataclass
 class train_config:
     batch_size: int = 128
@@ -8,5 +5,7 @@ class train_config:
     mixed_precision: bool = True
     host_port: str = "12399"
     activation_checkpointing: bool = True
-    num_epochs: int = 4
-    dataset_train = "datasets_grammar/gtrain_500k.csv"
+    num_epochs: int = 2
+    dataset_train = "datasets_grammar/gtrain_100k.csv"  # grammar_13k.csv
+    dataset_test = "datasets_grammar/grammar_validation.csv"
+    model_checkpoint = "t5large_2e.pt"
