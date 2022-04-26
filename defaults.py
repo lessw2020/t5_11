9cce4bd938f5db183abb9f023589622ad86ba064
lessw2020 <lessw@etrillium.com>
2022-04-26 04:29:37 +0000

    make defaults cfg more readable with comment spacing

--- defaults.py
+++ defaults.py
@@ -3,14 +3,23 @@ from dataclasses import dataclass
 
 @dataclass
 class train_config:
-    batch_size: int = 16
-    fsdp_unit_size = 2000000
-    save_model: bool = True
-    mixed_precision: bool = True
+    # general
     host_port: str = "12399"
+
+    # model
+    model_name = "t5-small"
+    save_model: bool = True
+    model_checkpoint = "t5large_2e.pt"
+
+    # policies
+    fsdp_unit_size = 2000000
+    mixed_precision: bool = True
     activation_checkpointing: bool = True
-    num_epochs: int = 2
+
+    # datasets
     dataset_train = "datasets_grammar/grammar_train.csv"  # grammar_13k.csv
     dataset_test = "datasets_grammar/grammar_validation.csv"
-    model_checkpoint = "t5large_2e.pt"
-    model_name = "t5-small"
+
+    # training
+    batch_size: int = 16
+    num_epochs: int = 2
