909acdbf2a51058b8d546aeb70955ccdddf28692
Less Wright <lessw@etrillium.com>
2022-08-25 17:48:33 -0700

    fix import

--- _comm_hooks/__init__.py
+++ _comm_hooks/__init__.py
@@ -1,6 +1,8 @@
 
 from . import default_hooks as default
 
+from .default_hooks import fp16_compress_hook, bf16_compress_hook
+
 LOW_PRECISION_HOOKS = [
     default.fp16_compress_hook,
     default.bf16_compress_hook,
