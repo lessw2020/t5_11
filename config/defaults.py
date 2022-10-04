d1898ff42a900db33c17c88db99cbad35bc20d6b
Hamid Shojanazeri <hamid.nazeri2010@gmail.com>
2022-10-04 06:31:51 -0700

    Update defaults.py

--- config/defaults.py
+++ config/defaults.py
@@ -74,7 +74,7 @@ class train_config:
     dataset_test = "datasets_grammar/grammar_validation.csv"
 
     # training
-    batch_size: int = 8
+    batch_size: int = 4
     num_epochs: int = 2
 
     # validation
