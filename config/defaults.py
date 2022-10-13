d95ed77617f2e67b5f1b2d78d50a0103d4c2d9a0
lessw2020 <lessw@etrillium.com>
2022-10-13 11:26:48 -0700

    bs=16

--- config/defaults.py
+++ config/defaults.py
@@ -73,7 +73,7 @@ class train_config:
     dataset_test = "datasets_grammar/grammar_validation.csv"
 
     # training
-    batch_size: int = 24
+    batch_size: int = 16
     num_epochs: int = 2
 
     # validation
